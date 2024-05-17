52f285773076ad1a0f10e3aa6b2056de2a99b94b
Tuon13141 <101558277+Tuon13141@users.noreply.github.com>
2024-05-17 18:13:36 +0700

    update UI scale to phone

---
+++ Assets/Scripts/Sound Scripts/VolumeSetting.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSetting : MonoBehaviour
+{
+    public Slider audioSlider;
+    public AudioMixer mainAudio;
+
+    public string audioName;
+
+    private void Start()
+    {
+        audioSlider.value = PlayerPrefs.GetFloat(audioName);
+        ChangeAudioVolume();
+    }
+    public void ChangeAudioVolume()
+    {
+        Debug.Log(PlayerPrefs.GetFloat(audioName));
+        float volume = 0;
+        if (audioSlider.value == 0)
+        {
+            volume = -80;
+        }
+        else
+        {
+            volume = Mathf.Log10(audioSlider.value) * 20;
+
+        }
+        mainAudio.SetFloat(audioName, volume);
+        PlayerPrefs.SetFloat(audioName, audioSlider.value);
+    }
+}
